f4f6e77eca60a767c10c2b263f32f1cb34143190
Felix Jacobi <felix.jacobi@iserv.eu>
2017-12-03 04:30:06 +0100

    Initial commit

---
+++ db/ipv6ndpwachtd.sql
@@ -0,0 +1,13 @@
+CREATE TABLE ipv6_ndp_log (
+    ID          SERIAL,
+    IP	        INET NOT NULL,
+    Mac         TEXT CHECK (mac ~
+	               '^([0-9a-f]{2}:){5}[0-9a-f]{2}$'),
+    First_Seen  TIMESTAMPTZ NOT NULL DEFAULT now(),
+    Last_Seen   TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE USER ipv6ndpwatchd;
+
+GRANT SELECT, USAGE ON ipv6_ndp_log_id_seq TO ipv6ndpwatchd;
+GRANT SELECT, INSERT, UPDATE ON ipv6_ndp_log TO ipv6ndpwatchd;
